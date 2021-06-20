f61aca5dbc97c09e2841f5d726dd1bf771e8eca5
fadihanna123 <fhanna181@gmail.com>
2021-06-20 22:33:44 +0200

    Make code cleaner

--- App.tsx
+++ App.tsx
@@ -12,20 +12,22 @@ const App = () => {
   const [view, setView] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
 
+  const PhotoAlbumProps = {
+    list: list,
+    setList: setList,
+    loading: loading,
+    setLoading: setLoading,
+    photo: photo,
+    setPhoto: setPhoto,
+    view: view,
+    setView: setView,
+  };
+
   return (
     <View style={styles.container}>
       <View>
         <GalleryTxt setView={setView} />
-        <PhotoAlbum
-          list={list}
-          setList={setList}
-          loading={loading}
-          setLoading={setLoading}
-          photo={photo}
-          setPhoto={setPhoto}
-          view={view}
-          setView={setView}
-        />
+        <PhotoAlbum {...PhotoAlbumProps} />
       </View>
       <StatusBar />
     </View>

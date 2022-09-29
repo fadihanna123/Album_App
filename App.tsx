22339e301d37c4769facfa16ad67adafd28a1bfe
FadiHanna <fadi.hanna@rektron.se>
2022-09-29 13:48:08 +0200

    Change folder structure

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import Layout from './app/Layout';
-import { store } from './redux/app';
+import Layout from './src/app/Layout';
+import { store } from './src/redux/app';
 
 const App: React.FC = () => (
   <Provider store={store}>
